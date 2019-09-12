872e69cecf989fc0f08b706c730ae12dc4de22b5
huoshan017 <huoshan017@github.com>
2019-09-12 21:32:16 +0800

    11111

--- db_define/build_all.bat
+++ db_define/build_all.bat
@@ -1,4 +1,4 @@
-set GOPATH=%cd%/../../..
-code_generator.exe -c json/game_db.json -d ../src/db_gen -p ../third_party/protobuf/protoc.exe
-code_generator.exe -c json/login_db.json -d ../src/db_gen -p ../third_party/protobuf/protoc.exe
-code_generator.exe -c json/rpc_db.json -d ../src/db_gen -p ../third_party/protobuf/protoc.exe
+set GOPATH=%cd%/../../../
+code_generator.exe -c json/game_db.json -d ../src/db_gen
+code_generator.exe -c json/login_db.json -d ../src/db_gen
+code_generator.exe -c json/rpc_db.json -d ../src/db_gen
